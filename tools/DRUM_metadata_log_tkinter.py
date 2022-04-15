8ed1d4a52f69dc73d6069943b957b6b693b19128
Melinda Kernik <kerni016@umn.edu>
2022-04-15 14:55:05 -0500

    Update DRUM_metadata_log_tkinter.py
    added urllib.request to imports

--- tools/DRUM_metadata_log_tkinter.py
+++ tools/DRUM_metadata_log_tkinter.py
@@ -6,8 +6,8 @@ inputs: -DRUM handle URL (i.e. https://hdl.handle.net/11299/226188 or https://co
         -Directory path for the metadata log
 output: text file metadata log ("metadata_log_[last 6 numbers of the handle]_")
 
-description: This tkinter script tool generates a metadata log based on the 
-information entered by researchers during the submission process to the Data 
+description: This tkinter script tool generates a metadata log based on the
+information entered by researchers during the submission process to the Data
 Repository for the University of Minnesota (DRUM) and the original files uploaded.
 
 last modified: April 2022
@@ -17,6 +17,7 @@ author: Melinda Kernik
 ##import necessary modules and return a message if any are not available
 try:
     import urllib
+    import urllib.request
     #import sys
     import math
     import tkinter
@@ -30,11 +31,11 @@ try:
 except Exception as e:
     print(e)
 
-def show_error(text): 
+def show_error(text):
     tkinter.messagebox.showerror('Error', text)
 
 def show_results(text):
-    tkinter.messagebox.showinfo("Results", text)    
+    tkinter.messagebox.showinfo("Results", text)
 
 def convert_size(size_bytes):
     """Convert file size in bytes to a more human readable format"""
@@ -49,59 +50,59 @@ def convert_size(size_bytes):
 
 
 def metadata_log(handle_url, outputDir):
-    
-    #Use the handle URL to construct a URL to get to the Dspace endpoint for the item  
+
+    #Use the handle URL to construct a URL to get to the Dspace endpoint for the item
     handle_split = handle_url.split ("/") [-2:]
     handle = str(handle_split[0]) + "/" + str(handle_split[1])
     url = "https://conservancy.umn.edu/rest/handle/" + handle
-    
+
     #Try to access the Dspace endpoint. Return an error message and exit the script if the URL cannot be opened.
     try:
         response = urllib.request.urlopen(url)
     except Exception as e:
         show_error(url + " could not be opened. (" + str(e) + ")")
-    
+
     #Read in the content at the endpoint and get the internal id for the item
     presoup = BeautifulSoup(response, 'lxml')
     item_info = presoup.p.text
     item_dict = eval(item_info.replace('null', '"null"'))
-    
+
     internal_id = item_dict["id"]
-    
-    
+
+
     ###Get item bitstream information from the submission
-    
-    #Read in the content at the bitstream endpoint. Default limit is 20 items per page.  
+
+    #Read in the content at the bitstream endpoint. Default limit is 20 items per page.
     #Extended to 250 to account for larger data submissions.
     url = "https://conservancy.umn.edu/rest/items/" + str(internal_id) + "/bitstreams?limit=250"
     response = urllib.request.urlopen(url)
     item_soup = BeautifulSoup(response, 'lxml')
     bitstream = item_soup.p.text
     list_bitstream = eval(bitstream.replace('null', '"null"'))
-    
+
     #Create the item bitstream section of the log
     bitstream_string = ""
     for x in list_bitstream:
         if x['bundleName'] == "ORIGINAL":
             bitstream_string += (x['name'] + " (" + convert_size(x['sizeBytes']) + ")\n")
-    
-    
+
+
     ###Get metadata from the submission
     #Use the internal id to create the URL endpoint needed to access the metadata
     url = "https://conservancy.umn.edu/rest/items/" + str(internal_id) + "/metadata"
-    
+
     #Read in the content at the metadata endpoint
     response = urllib.request.urlopen(url)
     soup = BeautifulSoup(response, 'lxml')
     metadata = soup.p.string
     list_metadata = eval(metadata.replace('null', '"null"'))
-    
+
     #Create the original metadata section of the log
     metadata_string = ""
     for x in range(len(list_metadata)):
         metadata_string += list_metadata[x]['key'] + " : " + list_metadata[x]['value'] +"\n"
-    
-    
+
+
     ###Add the bitstream and metadata lists to the template metadata log text
     metadata_log_template = "Metadata log created: " + str(datetime.now().strftime("%Y-%m-%d")) + """
 \n*************************************************
@@ -126,11 +127,11 @@ Other issues
 *************************************************
 Original Metadata from Author:
 *************************************************\n"""  + metadata_string
-    
-    
+
+
     #Write the metadata log to a text file
     metadata_log_path = outputDir + "/metadata_" + str(handle_split[1]) + "_" + str(datetime.now().strftime("%Y%m%d")) + ".txt"
-    f = open(metadata_log_path,"w") 
+    f = open(metadata_log_path,"w")
     f.write(metadata_log_template)
     f.close()
     show_results("Finished creating metadata file for: " + handle_url)
@@ -167,7 +168,7 @@ text.pack()
 # Draw box for entering the handle URL
 entry = ttk.Entry(app, width = 70)
 entry.pack(ipady=2)
-                     
+
 # Draw the button that opens the file picker.
 open_folder = tkinter.Button(app, text="Choose folder for output", command=clicked)
 open_folder.pack(ipady=2, side = "bottom", expand = "True")
